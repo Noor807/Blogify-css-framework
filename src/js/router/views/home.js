29ae4a5c3059cd09b552615e487fb503f12b7b68
Noor Irfan <152153984+Noor807@users.noreply.github.com>
2024-11-21 12:38:36 +0100

    search bar function and tag handling

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -11,7 +11,20 @@ setLogoutListener();
 
 // Initial fetch to load the first page
 fetchPosts();  // Loads the first page by default
-
+const searchBar = document.querySelector('#searchInput');
+if(searchBar){
+    searchBar.addEventListener('keyup' , ()=>{
+        const tag = searchBar.value.trim()
+        if (tag==='') {
+            fetchPosts()   
+        } else {
+            fetchPosts(1, tag) 
+            
+        }
+    })
+}else{
+    console.error('searchbar not found')
+}
 // Set up the event listeners for the pagination buttons
 document.getElementById('left').addEventListener('click', goLeft);
 document.getElementById('right').addEventListener('click', goRight);
